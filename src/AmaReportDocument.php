35b8c9052052495df17da5d54e36e7ed6f293640
phpdie <sundaysunsun@vip.qq.com>
2023-01-13 10:08:52 +0800

    增加了解析报告文档的类

---
+++ src/AmaReportDocument.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace Phpdie\AmazonSpApi;
+
+
+class AmaReportDocument
+{
+    public static function parse($url, $key, $iv, $gzip = false)
+    {
+        $content = openssl_decrypt(
+            file_get_contents($url),
+            'AES-256-CBC',
+            base64_decode($key),
+            OPENSSL_RAW_DATA,
+            base64_decode($iv)
+        );
+        return $gzip ? gzdecode($content) : $content;
+    }
+}
