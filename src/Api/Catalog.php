16d9aa0fdd69a537646592a79144d9fc4ea162f6
phpdie <sundaysunsun@vip.qq.com>
2023-01-14 10:13:39 +0800

    增加了Catalog

---
+++ src/Api/Catalog.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Phpdie\AmazonSpApi\Api;
+
+use Phpdie\AmazonSpApi\AmaRequest;
+
+class Catalog
+{
+    private AmaRequest $instance;
+
+    public function __construct(AmaRequest $instance)
+    {
+        $this->instance = $instance;
+    }
+
+    public function searchCatalogItems(array $param)
+    {
+        $path = '/catalog/2022-04-01/items';
+        return $this->instance->sendRequest($path, $param, [], 'GET');
+    }
+
+    public function getCatalogItem(array $param)
+    {
+        $asin = $param['asin'];
+        $path = '/catalog/2022-04-01/items/{asin}';
+        $path = str_replace('{asin}', $asin, $path);
+        unset($param['asin']);
+        return $this->instance->sendRequest($path, $param, [], 'GET');
+    }
+}
