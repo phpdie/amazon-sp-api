91b3119ac44de31c2efad1fc55000305ef86a590
phpdie <sundaysunsun@vip.qq.com>
2023-02-07 15:48:52 +0800

    add document download

--- src/AmaReportDocument.php
+++ src/AmaReportDocument.php
@@ -4,15 +4,29 @@ namespace AmazonSpApi;
 
 class AmaReportDocument
 {
-    public static function parse($url, $key, $iv, $gzip = false)
+    public static function parse(string $url, string $key = '', string $iv = '', bool $gzip = false)
     {
-        $content = openssl_decrypt(
-            file_get_contents($url),
-            'AES-256-CBC',
-            base64_decode($key),
-            OPENSSL_RAW_DATA,
-            base64_decode($iv)
-        );
+        if (empty($key) && empty($iv)) {
+            $content = file_get_contents($url);
+        } else {
+            $content = openssl_decrypt(
+                file_get_contents($url),
+                'AES-256-CBC',
+                base64_decode($key),
+                OPENSSL_RAW_DATA,
+                base64_decode($iv)
+            );
+        }
         return $gzip ? gzdecode($content) : $content;
+    }
+
+    public static function download(string $url, string $key = '', string $iv = '', bool $gzip = false)
+    {
+        $data = self::parse($url, $key, $iv, $gzip);
+        header("Content-Type: application/vnd.ms-excel; charset=utf-8");
+        header("Content-Disposition: attachment; filename=document.xls");
+        header("Pragma: no-cache");
+        header("Expires: 0");
+        echo $data;
     }
 }
