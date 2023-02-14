2bf5c3f75f87d7f79efd2cbd3a0f278070847c95
phpdie <sundaysunsun@vip.qq.com>
2023-02-14 15:47:40 +0800

    文档的参数优化

--- src/AmaReportDocument.php
+++ src/AmaReportDocument.php
@@ -4,31 +4,21 @@ namespace AmazonSpApi;
 
 class AmaReportDocument
 {
-    public static function parse(string $url, bool $gzip = false, string $key = '', string $iv = '')
+    public static function parse(string $url, bool $gzip = false)
     {
-        if (empty($key) && empty($iv)) {
-            $content = file_get_contents($url);
-        } else {
-            $content = openssl_decrypt(
-                file_get_contents($url),
-                'AES-256-CBC',
-                base64_decode($key),
-                OPENSSL_RAW_DATA,
-                base64_decode($iv)
-            );
-        }
-        return $gzip ? gzdecode($content) : $content;
+        $report_content = file_get_contents($url);
+        return $gzip ? gzdecode($report_content) : $report_content;
     }
 
-    public static function download(string $data, $filename = 'document.xls', callable $func = null)
+    public static function download(string $report_content, $filename = 'document.xls', callable $func = null)
     {
         header("Content-Type: application/vnd.ms-excel; charset=utf-8");
         header("Content-Disposition: attachment; filename=$filename");
         header("Pragma: no-cache");
         header("Expires: 0");
         if ($func) {
-            $data = $func($data);
+            $report_content = $func($report_content);
         }
-        echo $data;
+        echo $report_content;
     }
 }
