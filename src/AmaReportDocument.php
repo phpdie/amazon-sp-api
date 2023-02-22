ee17660289c4b3b8dd7fa60a387995024229cf68
phpdie <sundaysunsun@vip.qq.com>
2023-02-22 14:17:19 +0800

    增加了转数组的功能

--- src/AmaReportDocument.php
+++ src/AmaReportDocument.php
@@ -4,21 +4,48 @@ namespace AmazonSpApi;
 
 class AmaReportDocument
 {
-    public static function parse(string $url, bool $gzip = false): string
+    public function parse(string $url, bool $gzip = false): string
     {
-        $report_content = file_get_contents($url);
-        return $gzip ? gzdecode($report_content) : $report_content;
+        $reportContent = file_get_contents($url);
+        return $gzip ? gzdecode($reportContent) : $reportContent;
     }
 
-    public static function download(string $report_content, $filename = 'document.xls', callable $func = null): void
+    public function download(string $reportContent, $filename = 'document.xls', callable $func = null): void
     {
         header("Content-Type: application/vnd.ms-excel; charset=utf-8");
         header("Content-Disposition: attachment; filename=$filename");
         header("Pragma: no-cache");
         header("Expires: 0");
         if ($func) {
-            $report_content = $func($report_content);
+            $reportContent = $func($reportContent);
         }
-        echo $report_content;
+        echo $reportContent;
+    }
+
+    public function toArray($result, $clearNullLine = false, $nl = '换行符', $tab = '制表符')
+    {
+        $temp = str_replace(array("\r", "\n"), $nl, $result);
+        $temp = rtrim($temp, $nl);
+        $temp = str_replace(array("\t"), $tab, $temp);
+        $t = [];
+        $arr = explode($nl, $temp);
+        $keyArr = [];
+        foreach ($arr as $k => $r) {
+            if ($k === 0) {
+                $keyArr = explode($tab, $r);
+            } else {
+                if ($clearNullLine && empty($r)) continue;
+                $value_arr = explode($tab, $r);
+                $l_key = count($keyArr);//键的个数
+                $l_value = count($value_arr);//值的个数
+                if ($l_key < $l_value) {
+                    $value_arr = array_slice($value_arr, 0, $l_key);
+                } else if ($l_key > $l_value) {
+                    $value_arr = array_pad($value_arr, $l_key, '');
+                }
+                $t[] = array_combine($keyArr, $value_arr);
+            }
+        }
+        return $t;
     }
 }
